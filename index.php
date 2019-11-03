90f79e26436f48f06d2c1cb7f813a34b22f4c6ef
Greg D <gregdykes@users.noreply.github.com>
2019-11-03 12:03:53 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
     <title>PHP Test</title>
   </head>
   <body>
-    <?php echo '<p>Hello World-1. This is the firt page.</p>' ?>
+    <?php echo '<p>Hello World-1. This is the first page.</p>' ?>
   </body>
 </html>
